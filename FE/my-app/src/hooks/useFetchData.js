2ed318a51c0898262f47e02ffe9f0da1a724f5fc
menglutao <azraeltml@gmail.com>
2023-09-23 23:27:35 +0800

    show data in the table

---
+++ FE/my-app/src/hooks/useFetchData.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+
+
+function useFetchData(url){
+
+    const [data, setData]   = useState(null);
+    const [loading,setLoading] = useState(true);
+    const [error,setError] = useState(null);
+    
+
+
+    useEffect(() => {
+        async function fetchData() {
+          try {
+            const response = await axios.get(url);
+            setData(response.data);
+            setLoading(false);
+            console.log("response:",response.data)
+            
+          } catch (err) {
+            setError(err);
+            setLoading(false);
+          }
+        }
+    
+        fetchData();
+      }, [url]);
+    
+      return { data, loading, error };
+    }
+
+export default useFetchData;
--- FE/my-app/src/pages/index.tsx
+++ FE/my-app/src/pages/index.tsx
@@ -1,14 +1,47 @@
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import useFetchData from '../hooks/useFetchData';
 
 const inter = Inter({ subsets: ['latin'] })
+const API_URL = 'https://my.api.mockaroo.com/shipments.json?key=5e0b62d0';
 
 export default function Home() {
+  const { data, loading, error } =  useFetchData(API_URL);
+  if(loading) return <div>Loading...</div>
+  if(error) return <div>Error: {error}</div>
+
+  if(data)
   return (
     <main>
-      <div>
-        Starting point
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>ORDERNO</th>
+            <th>DELIVERYDATE</th>
+            <th>CUSTOMER</th>
+            <th>TRACKINGNO</th>
+            <th>STATUS</th>
+            <th>CONSIGNEE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item,index)=>(
+            <tr key={index}>
+              <td>{item.orderNo}</td>
+              <td>{item.date}</td>
+              <td>{item.customer}</td>
+              <td>{item.trackingNo}</td>
+              <td>{item.status}</td>
+              <td>{item.consignee}</td>
+
+            </tr>
+          )
+          )}
+        </tbody>
+      
+  
+      </table>
     </main>
 
   )
 }
+
