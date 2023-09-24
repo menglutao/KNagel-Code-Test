5e53a10f92c25e8a06a1e48fee46472f6ff4b90a
menglutao <azraeltml@gmail.com>
2023-09-25 00:56:06 +0800

    update css

--- FE/my-app/src/pages/index.tsx
+++ FE/my-app/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import useFetchData from '../hooks/useFetchData';
 
+
 const inter = Inter({ subsets: ['latin'] })
 const API_URL = 'https://my.api.mockaroo.com/shipments.json?key=5e0b62d0';
 
@@ -9,43 +10,51 @@ export default function Home() {
   if (loading) return <div>Loading...</div>
   if (error) return <div>Error: {error}</div>
 
-    return (
-      <main>
-        <div className="flex flex-col">
-          <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-              <div className="overflow-hidden">
-                <table className="min-w-full text-left text-sm font-light">
-                  <thead className="border-b font-medium dark:border-neutral-500">
-                    <tr>
-                      <th>ORDERNO</th>
-                      <th>DELIVERYDATE</th>
-                      <th>CUSTOMER</th>
-                      <th>TRACKINGNO</th>
-                      <th>STATUS</th>
-                      <th>CONSIGNEE</th>
-                    </tr>
-                  </thead>
-                  <tbody> 
-                    {data && data.map((item, index) => (
-                      <tr className="border-b dark:border-neutral-500" key={index}>
-                        <td className="whitespace-nowrap px-6 py-4">{item.orderNo}</td>
-                        <td className="whitespace-nowrap px-6 py-4">{item.date}</td>
-                        <td className="whitespace-nowrap px-6 py-4">{item.customer}</td>
-                        <td className="whitespace-nowrap px-6 py-4">{item.trackingNo}</td>
-                        <td className="whitespace-nowrap px-6 py-4">{item.status}</td>
-                        <td className="whitespace-nowrap px-6 py-4">{item.consignee}</td>
-                      </tr>)
-                    )}
-                  </tbody>
-                </table>
-              </div>
+  return (
+    <main>
+      <div className="mt-2 flex flex-col">
+        <div className="-my-2 overflow-x-auto -mx-4 sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200 content-center">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ORDERNO</th>
+                    <th scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">DELIVERYDATE</th>
+                    <th scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">CUSTOMER</th>
+                    <th scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">TRACKINGNO</th>
+                    <th scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">STATUS</th>
+                    <th scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">CONSIGNEE</th>
+                    <th scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">xxx </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data && data.map((item, index) => (
+                    <tr className="px-6 py-4 whitespace-nowrap" key={index}>
+                      <td className="px-6 py-4 whitespace-nowrap">{item.orderNo}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">{item.date}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">{item.customer}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">{item.trackingNo}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">{item.status}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">{item.consignee}</td>
+                    </tr>)
+                  )}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-      </main>
-    )
-  }
+      </div>
+    </main>
+  )
+}
 
 
 
